f8a9b53c356bde968e236783e4cfc79b19dfbc2a
Enso Cejas <127504333+efcejas@users.noreply.github.com>
2025-03-16 14:43:18 -0300

    Agrega archivos de configuración y recursos estáticos para la administración de Django

--- gestion_pacientes/gestion_pacientes/settings.py
+++ gestion_pacientes/gestion_pacientes/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Archivos multimedia (para imágenes, documentos subidos por usuarios)
 MEDIA_URL = '/media/'
