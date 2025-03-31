ad274753c0bac6a675a4962516c7c1a81ef3201c
Enso Cejas <127504333+efcejas@users.noreply.github.com>
2025-03-30 22:19:31 -0300

    Agrega funcionalidad para cambiar la contraseña de usuario y mejora las plantillas de autenticación

--- gestion_pacientes/usuarios/forms.py
+++ gestion_pacientes/usuarios/forms.py
@@ -1,6 +1,10 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 from .models import Usuario
+from django.contrib.auth.password_validation import password_validators_help_text_html
+from django.contrib.auth import get_user_model
+
+# Formulario para el registro de usuarios
 
 class RegistroUsuarioForm(UserCreationForm):
     rol = forms.ChoiceField(choices=Usuario.ROLES, label="Rol")
@@ -8,3 +12,59 @@ class RegistroUsuarioForm(UserCreationForm):
     class Meta:
         model = Usuario
         fields = ["username", "email", "rol", "password1", "password2"]
+        
+# Formulario para la autenticación de usuarios
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        label="",  # No mostrar el label
+        max_length=150,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',  # Clase de Bootstrap
+            'placeholder': 'Ingrese su usuario',  # Placeholder
+            'autofocus': True,  # Enfocar automáticamente este campo
+        })
+    )
+    password = forms.CharField(
+        label="",  # No mostrar el label
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',  # Clase de Bootstrap
+            'placeholder': 'Ingrese su contraseña',  # Placeholder
+        })
+    )
+
+    class Meta:
+        model = Usuario
+        fields = ['username', 'password']
+
+Usuario = get_user_model()
+
+class CambioContrasenaForm(PasswordChangeForm):
+    old_password = forms.CharField(
+        label="",
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ingrese su contraseña actual',
+        })
+    )
+
+    new_password1 = forms.CharField(
+        label="",
+        help_text=password_validators_help_text_html(),  # HTML con <ul><li>...
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ingrese su nueva contraseña',
+        })
+    )
+
+    new_password2 = forms.CharField(
+        label="",
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Confirme su nueva contraseña',
+        })
+    )
+
+    class Meta:
+        model = Usuario
+        fields = ['old_password', 'new_password1', 'new_password2']
--- gestion_pacientes/usuarios/urls.py
+++ gestion_pacientes/usuarios/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views import RegistroUsuarioView, HomeView
 from django.contrib.auth import views as auth_views
+from .forms import LoginForm, CambioContrasenaForm
 
 urlpatterns = [
-    path("registro/", RegistroUsuarioView.as_view(), name="registro"),
-    path("login/", auth_views.LoginView.as_view(template_name="usuarios/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", HomeView.as_view(), name="home"),
+    
+    # Rutas relacionadas con el registro y authentication de usuarios
+    path("registro/", RegistroUsuarioView.as_view(), name="registro"),
+    path("login/", auth_views.LoginView.as_view(template_name="usuarios/login.html", authentication_form=LoginForm), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("password_change/", auth_views.PasswordChangeView.as_view(template_name="usuarios/password_change.html", form_class=CambioContrasenaForm), name="password_change"),
+    path("password_change/done/", auth_views.PasswordChangeDoneView.as_view(template_name="usuarios/password_change_done.html"), name="password_change_done"),
 ]
