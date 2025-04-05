84d58c3078c314fd7f10634aaf82939360e6bfb4
Enso Cejas <127504333+efcejas@users.noreply.github.com>
2025-04-04 21:23:03 -0300

    Actualiza la configuración de archivos estáticos para utilizar Whitenoise y define el directorio de archivos estáticos

--- config/settings.py
+++ config/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Archivos multimedia (para imágenes, documentos subidos por usuarios)
