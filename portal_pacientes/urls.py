ebd8c518fec556a221009277390e019baf489842
Enso Cejas <127504333+efcejas@users.noreply.github.com>
2025-05-30 19:34:51 -0300

    Add "Quiénes Somos" and "Ubicación y Contacto" pages to patient portal
    - Created quienes_somos.html to provide information about the medical center, including team commitment and facilities.
    - Created ubicaciones_y_contactos.html to display contact information and an embedded Google Map for location.
    - Both pages extend the base template and include a button to return to the home page.

--- portal_pacientes/urls.py
+++ portal_pacientes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from portal_pacientes.views import HomePacientesView
+from portal_pacientes.views import QuienesSomosView, UbicacionesYContactosView, HomePacientesView
 
 urlpatterns = [
     path('', HomePacientesView.as_view(), name='home_pacientes'),
+    path('quienes-somos/', QuienesSomosView.as_view(), name='quienes_somos'),
+    path('ubicaciones-y-contactos/', UbicacionesYContactosView.as_view(), name='ubicaciones_y_contactos'),
 ]
--- portal_pacientes/views.py
+++ portal_pacientes/views.py
@@ -3,3 +3,10 @@ from django.views.generic import TemplateView
 
 class HomePacientesView(TemplateView):
     template_name = "portal_pacientes/home.html"
+    
+class QuienesSomosView(TemplateView):
+    template_name = "portal_pacientes/quienes_somos.html"
+    
+class UbicacionesYContactosView(TemplateView):
+    template_name = "portal_pacientes/ubicaciones_y_contactos.html"
+    
