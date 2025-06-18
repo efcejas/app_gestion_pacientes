0d853f8938ad3840ee1249ddfbb69f1f0dd73812
Enso Cejas <127504333+efcejas@users.noreply.github.com>
2025-06-17 23:43:43 -0300

    Actualiza la funcionalidad de renovación de órdenes médicas: modifica las rutas y la lógica para manejar la renovación, incluyendo un nuevo modal y ajustes en la interfaz.

--- control_ordenes/urls.py
+++ control_ordenes/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import OrdenMedicaAnonimaCreateView, OrdenesDelMedicoListView, marcar_orden_renovada
+from .views import OrdenMedicaAnonimaCreateView, OrdenesDelMedicoListView
+from control_ordenes import views
 
 app_name = 'control_ordenes'
 
 urlpatterns = [
     path('nueva/', OrdenMedicaAnonimaCreateView.as_view(), name='crear_orden'),
     path('mis-ordenes/', OrdenesDelMedicoListView.as_view(), name='lista_ordenes'),
-    path('marcar_renovada/<int:pk>/', marcar_orden_renovada, name='marcar_renovada'),
+    path('renovar/<int:orden_id>/', views.renovar_orden, name='renovar_orden'),
 ]
